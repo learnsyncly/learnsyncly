cf9a2eaaf10e76a246b409e67cf8a34015628607
Jefri Vanegas <vjefri@gmail.com>
2016-01-07 20:12:11 -0800

    (feat) schema for mongo database, user and presentation

---
+++ src/server/mongoSchema.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create user schema
+var userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  creator: Boolean,
+  presentations: Array
+});
+
+// create presentation schema
+var presentationSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  length: Number,
+  slides: [{
+    timestamp: Date,
+    slide: Number
+  }]
+});
+
+var User = mongoose.model('User', userSchema);
+var Presentation = mongoose.model('Presentation', presentationSchema);
+
+module.exports = User;
+module.exports = Presentation;
