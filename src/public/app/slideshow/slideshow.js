f80eca2e6b318f6c6428293b6aba5862feed692e
Adam Dobkin <adam.dobkin@gmail.com>
2016-01-09 01:57:18 -0500

    Basic slideshow proof of concept built. Also fixed async gulp build issues.

--- src/public/app/slideshow/slideshow.js
+++ src/public/app/slideshow/slideshow.js
@@ -1,6 +1,16 @@
 //slideshow controller
 angular.module('lsync.slideshow', [])
+.controller('SlideshowController', function($scope, $rootScope, SlideshowState, $sce) {
+  $scope.data = SlideshowState;
+  console.log($scope);
+  var updateSlideShow = function() {
+    $scope.data.fullUrl = $sce.trustAsResourceUrl($scope.data.baseUrl + $scope.data.slideNumber);
+    $scope.$apply();
+  };
+  $scope.data.fullUrl = $sce.trustAsResourceUrl($scope.data.baseUrl + $scope.data.slideNumber);
 
-.controller('SlideshowController', function($scope){
-
+  //$rootScope.on allows messaging between factory and view
+  //useful if our controller needs signaling from a factory
+  //factory uses $rootScope.$emit to prevent messages from traversing nested $scopes
+  $rootScope.$on('slideDataChange', updateSlideShow);
 });
