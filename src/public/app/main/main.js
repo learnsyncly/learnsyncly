a673b6c2a97ebe5703ff5674a2bacfaf6ef91032
Zan Rosenthal <zan.rosenthal@gmail.com>
2016-01-11 00:55:08 -0500

    Implemented nested views. Continued working on emitting events from video player.

--- src/public/app/main/main.js
+++ src/public/app/main/main.js
@@ -4,5 +4,6 @@ angular.module('lsync.main', [])
 
   // This controller will hold the main app logic and can display slightly
   // different options depending on if it's in create or view mode.
-
+  $state.go('main.container');
+  $stat.go('video');
 });
