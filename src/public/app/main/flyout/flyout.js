dc05777789f4225ecfaff57bdc84db7bf0c8e2e9
Adam Dobkin <adam.dobkin@gmail.com>
2016-01-11 11:16:15 -0500

    UI Router now working and basic ui structure in place.

--- src/public/app/main/flyout/flyout.js
+++ src/public/app/main/flyout/flyout.js
@@ -1,5 +1,8 @@
 //flyout controller
 angular.module('lsync.flyout', [])
-.controller('FlyoutController', function($scope) {
-
+.controller('FlyoutController', function($scope, AppState) {
+  angular.extend($scope, AppState);
+  $scope.toggleFlyout = function(){
+    $scope.flyout.status = !$scope.flyout.status;
+  };
 });
