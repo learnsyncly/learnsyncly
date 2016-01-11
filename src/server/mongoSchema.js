28b8f0177c555c55df46a74576c5e83d7be16c55
Jefri Vanegas <vjefri@gmail.com>
2016-01-11 11:17:55 -0800

    (fix) made changes to login and create form

--- src/server/mongoSchema.js
+++ src/server/mongoSchema.js
@@ -3,23 +3,34 @@ var Schema = mongoose.Schema;
 
 // create user schema
 var userSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   creator: Boolean,
   presentations: Array
 });
 
 // create presentation schema
 var presentationSchema = new Schema({
-  name: { type: String, required: true, unique: true },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
   length: Number,
-  videos:[{
-    aspectRatio: Number
+  videos: [{
+    aspectRatio: String
   }],
   slides: [{
     timestamp: Date,
     slide: Number,
-    aspectRatio: Number
+    aspectRatio: String
   }]
 });
 
