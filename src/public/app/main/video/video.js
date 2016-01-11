443c32177a37a2ddb3dd5a3ea26af6b9e085601d
Zan Rosenthal <zan.rosenthal@gmail.com>
2016-01-11 18:29:39 -0500

    Working on setting video to zero at start

--- src/public/app/main/video/video.js
+++ src/public/app/main/video/video.js
@@ -1,32 +1,31 @@
 angular.module('lsync.video', ['lsync.services'])
-.controller('VideoController', function($scope){
-
-  $scope.alertCurrentTime = function(){
-    console.log($scope.player.getCurrentTime());
+.controller('VideoController', function($scope,VideoState){
+  angular.extend($scope,VideoState);
+  $scope.playerEvent = function(event){
+    var data = Array.prototype.slice.apply(arguments, 1);
+    this.$broadcast(event,data);
   };
 
   $scope.url='https://www.youtube.com/watch?v=lBqiZSemrqg&ab_channel=TimKindberg';
+  $scope.$on('updateTime', function (event, data) {
+    console.log(data);
+  });
 
-  $scope.alertCurrentTime = function(){
-    console.log($scope.player.getCurrentTime());
-  };
   $scope.data={
-    width: 600,
-    height: 480,
-    videoid: "M7lc1UVf-VE"
+    videoid: "M7lc1UVf-VE",
   };
 })
-.directive('youtube', function($window) {
+.directive('youtube', function($window,$rootScope) {
   return {
     restrict: "E",
 
     scope: {
-      height: "@",
-      width: "@",
-      videoid: "@"
+      videoid: "@",
     },
 
     template: '<div></div>',
+
+    player: null,
 
     link: function(scope, element) {
       var tag = document.createElement('script');
@@ -34,36 +33,37 @@ angular.module('lsync.video', ['lsync.services'])
       var firstScriptTag = document.getElementsByTagName('script')[0];
       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      var player;
-
       $window.onYouTubeIframeAPIReady = function() {
 
         player = new YT.Player(element.children()[0], {
           playerVars: {
-            autoplay: 1,
-            html5: 1,
-            theme: "light",
-            modesbranding: 0,
+            autoplay: 0,
+            theme: "dark",
             color: "white",
             iv_load_policy: 3,
             showinfo: 1,
-            controls: 1
+            controls: 1,
+            start: 0,
           },
 
-          height: scope.height,
-          width: scope.width,
           videoId: scope.videoid,
 
           events:{
+            'onReady': seekToBeginning,
             'onStateChange':onPlayerStateChange
-          }
+         }
         });
+        seekToBeginning();
+        setInterval(onPlayerStateChange,500);
       };
-
+      var seekToBeginning = function(){
+        player.seekTo(0);
+        player.playVideo();
+      };
       var onPlayerStateChange = function(){
-        console.log('CURRENT TIME', player.getCurrentTime());
+        $rootScope.$emit('videoPlaying', player.getCurrentTime());
       };
-      setInterval(onPlayerStateChange, 500);
+      // setInterval(onPlayerStateChange, 500);
       scope.$watch('videoid', function(newValue, oldValue) {
         if (newValue == oldValue) {
           return;
@@ -72,14 +72,15 @@ angular.module('lsync.video', ['lsync.services'])
         player.cueVideoById(scope.videoid);
 
       });
-
-      scope.$watch('height + width', function(newValue, oldValue) {
-        if (newValue == oldValue) {
-          return;
-        }
-
-        player.setSize(scope.width, scope.height);
-
+      scope.$on('seekTo', function(time){
+        player.seekTo(time);
+        player.pauseVideo();
+      });
+      scope.$on('play',function(){
+        player.playVideo();
+      });
+      scope.$on('pause',function(){
+        player.pauseVideo();
       });
     }
   };
