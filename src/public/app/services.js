1d6742df9c832730260bc9a8b5a1236e5516f187
Adam Dobkin <adam.dobkin@gmail.com>
2016-01-10 20:26:02 -0500

    Overhauled directory structure and added all assets to the app config!

--- src/public/app/services.js
+++ src/public/app/services.js
@@ -1,36 +1,52 @@
 angular.module('lsync.services', [])
-.factory('AppState', function(SlideState, VideoState, UserState){
-  appState = {};
-  appState.slide = SlideState;
-  appState.video = VideoState;
-  appState.user = UserState;
-  //store app state data here
-  return appState;
-})
-.factory('UserState', function(){
-  //store video state data here
-  return {};
-})
-.factory('VideoState', function($rootScope){
+  .factory('AppState', function(Auth, SlideState, VideoState, UserState) {
+    appState = {};
+    appState.slide = SlideState;
+    appState.video = VideoState;
+    appState.user = UserState;
+    //store app state data here
+    return appState;
+  })
+  .factory('Auth', function() {
+    //store video state data here
+    return {};
+  })
+  .factory('UserState', function() {
+    //store video state data here
+    return {};
+  })
+  .factory('VideoState', function($rootScope) {
 
-  return {};
-})
-.factory('SlideState', function($rootScope, $sce){
-  //initial properties
-  var slide = {};
+    return {};
+  })
+  .factory('SlideState', function($rootScope, $sce, $http) {
+    //initial properties
+    var slide = {};
 
-  //data object to help namespace scope when extended on controller
-  slide.data = {};
-  slide.data.aspectRatio = 'aspect16-9';
-  slide.data.baseUrl = 'https://docs.google.com/presentation/d/1BrXgyVVKE02KvH8AcyHAs8KK-n1_mk3517uI5bXeOvw/embed?#slide=';
-  slide.data.slideNumber = 0;
-  slide.data.url = $sce.trustAsResourceUrl(slide.data.baseUrl + slide.data.slideNumber);
+    //data object to help namespace scope when extended on controller
+    slide.data = {};
+    slide.data.aspectRatio = 'aspect16-9';
+    slide.data.identifier = '1BrXgyVVKE02KvH8AcyHAs8KK-n1_mk3517uI5bXeOvw';
+    slide.data.identifier = '1BrXgyVVKE02KvH8AcyHAs8KK-n1_mk3517uI5bXeOvw';
+    slide.data.baseUrl = 'https://docs.google.com/presentation/d/' + slide.data.identifier + '/embed?#slide=';
+    slide.data.slideNumber = 0;
+    slide.data.url = $sce.trustAsResourceUrl(slide.data.baseUrl + slide.data.slideNumber);
 
-  //methods accessable from SlideState
-  slide.setSlide = function(number){
-    slide.data.slideNumber = number;
-    console.log(slide.data.slideNumber);
-  };
+    $http({
+      method: 'GET',
+      url: slide.data.baseUrl
+    }).then(function successCallback(response) {
+      console.log('good resource');
+    }, function errorCallback(response) {
+      console.log('bad resource');
+    });
 
-  return slide;
-});
+    //methods accessable from SlideState
+    slide.setSlide = function(number) {
+      slide.data.slideNumber = number;
+      console.log(slide.data.slideNumber);
+      return $sce.trustAsResourceUrl(slide.data.baseUrl + slide.data.slideNumber);
+    };
+
+    return slide;
+  });
