6e4fddf3962807bd7bf930d258df645e25c4dc5f
Adam Dobkin <adam.dobkin@gmail.com>
2016-01-10 20:26:02 -0500

    Overhauled directory structure and added all assets to the app config!

--- src/public/app/app.js
+++ src/public/app/app.js
@@ -1,11 +1,19 @@
+//require statements are loaded via browserify
+//if browserify doesn't support a file, load it with file path
 //angular dependencies
 require('angular');
 require('angular-ui-router');
-require('./services.js');
-require('./entry/entry.js');
-require('./video/video.js');
-require('./slide/slide.js');
+require('./auth/auth.js');
+require('./create/create.js');
 require('./main/main.js');
+require('./main/container/container.js');
+require('./main/container/slide/slide.js');
+// require('./main/container/video/video.js');
+require('./main/flyout/flyout.js');
+require('./main/toolbar/toolbar.js');
+require('./services.js');
+//TODO: swap item with new path above
+require('./video/video.js');
 
 //other dependencies
 require('../../../node_modules/socket.io-client/socket.io.js');
@@ -14,11 +22,14 @@ require('../../../node_modules/jquery/dist/jquery.js');
 //LearnSyncly
 angular.module('lsync', [
   'ui.router',
-  'lsync.services',
-  'lsync.entry',
-  'lsync.video',
+  'lsync.auth',
+  'lsync.create',
+  'lsync.main',
+  'lsync.container',
+  'lsync.flyout',
   'lsync.slide',
-  'lsync.main'
+  'lsync.services',
+  'lsync.toolbar'
 ])
 .config(function($stateProvider, $urlRouterProvider) {
   // $urlRouterProvider.otherwise('/');
@@ -33,11 +44,6 @@ angular.module('lsync', [
       templateUrl: 'app/auth/register.html',
       controller: 'AuthController'
     })
-    .state('entry', {
-      url: '/',
-      templateUrl: 'app/entry/entry.html',
-      controller: 'EntryController'
-    })
     .state('create', {
       abstract:true,
       url: '/create',
@@ -46,12 +52,25 @@ angular.module('lsync', [
     })
     .state('create.video', {
       url: '',
+      //TODO: update me later
+      // templateUrl: 'app/main/container/video/video.html',
       templateUrl: 'app/video/video.html',
       controller: 'VideoController'
     })
     .state('create.slide', {
       url: '',
-      templateUrl: 'app/slide/slide.html',
+      templateUrl: 'app/main/container/slide/slide.html',
+      controller: 'SlideController'
+    })
+
+
+
+
+
+    ////TODO REMOVE ONCE ROUTES ARE WORKING!
+    .state('slide', {
+      url: '/slide',
+      templateUrl: 'app/main/container/slide/slide.html',
       controller: 'SlideController'
     });
  });
---
+++ src/public/app/main/flyout/flyout.js
@@ -0,0 +1,5 @@
+//flyout controller
+angular.module('lsync.flyout', [])
+.controller('FlyoutController', function($scope) {
+
+});
