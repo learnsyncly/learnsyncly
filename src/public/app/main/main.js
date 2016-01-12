ca665c0d6b004b5a1aa2a08664409680b314e9c0
Zan Rosenthal <zan.rosenthal@gmail.com>
2016-01-12 10:56:17 -0500

    Tied video time and slidechanges as well as class transitions between video being prominent and slides being prominent.

--- src/public/app/main/main.js
+++ src/public/app/main/main.js
@@ -1,8 +1,10 @@
 //main controller
 angular.module('lsync.main', [])
-    .controller('MainController', function($scope, AppState) {
+    .controller('MainController', function($scope,$rootScope, AppState) {
   angular.extend($scope, AppState);
   // This controller will hold the main app logic and can display slightly
   // different options depending on if it's in create or view mode.
-      $scope.socket.connect();
+  $scope.socket.connect();
+  $scope.checkVars = function(){$scope.$apply();};
+  setInterval($scope.checkVars, 500);
 });
--- src/public/app/services.js
+++ src/public/app/services.js
@@ -1,11 +1,12 @@
 angular.module('lsync.services', [])
-  .factory('AppState', function(Auth, SlideState, VideoState, UserState, SocketState, PresentationState) {
-
+  .factory('AppState', function($interval, Auth, SlideState, VideoState, UserState, SocketState, PresentationState) {
+  
     appState = {};
     appState.slide = SlideState;
     appState.video = VideoState;
     appState.user = UserState;
     appState.socket = SocketState;
+    appState.presentation = PresentationState;
     //flyout status nested object for easy extending etc
     appState.data = {};
     appState.data.player = false;
@@ -25,30 +26,30 @@ angular.module('lsync.services', [])
       }
     };
 
-  appState.setTimeIndex = function(index){
-    presentation.timeIndex=index;
-  };
-  appState.checkTime = function(){
-    if(appState.video.data.playing){
-      if(appState.video.data.currentTime>=presentation.timestamps[presentation.timeIndex]){
-        presentation.timeIndex++;
-        appState.video.pause();
-        appState.toggleSlideView();
-        appState.slide.next();
+    appState.setTimeIndex = function(index){
+      appState.presentation.data.timeIndex=index;
+    };
+    appState.checkTime = function(){
+      if(appState.video.data.playing){
+        if(appState.video.data.currentTime>=appState.presentation.data.timestamps[appState.presentation.data.timeIndex]){       
+          appState.presentation.data.timeIndex++;
+          appState.toggleSlideView();
+          appState.slide.setSlide(appState.presentation.data.timeIndex);
+        }
       }
-    }
-  };
- setInterval(appState.checkTime, 500);
+    };
+    setInterval(appState.checkTime, 500);
 
     //store app state data here
     return appState;
   })
 .factory('PresentationState', function(){
- presentation={
-  timestamps:[5,10,15,20,25,30],
+ presentation={};
+ presentation.data={
+  timestamps:[5,10,15,20,25,30,35,40,45,50,55,60],
   // timestamps:[15,30,45,60,75,90],
   slides:[1,2,3,4,5,6],
-  timeIndex:0
+  timeIndex:1
  };
 
 
@@ -84,17 +85,16 @@ angular.module('lsync.services', [])
     video.data.aspectRatio = 'aspect16-9';
     video.data.identifier = 'M7lc1UVf-VE';
     video.data.url = '';
-    video.data.unPlayed=true;
 
     ytPlayerInit = function(event) {
       video.data.videoReady = true;
       video.seekTo(1);
+      video.data.playing=true;
       setInterval(updatePlayer, 1000);
     };
 
     var updatePlayer = function() {
       video.data.currentTime = player.getCurrentTime();
-      console.log('new current time',video.data.currentTime);
     };
 
     video.play = function() {
@@ -164,7 +164,6 @@ angular.module('lsync.services', [])
     if (slide.data.slideNumber + 1 > slide.data.length) {
       return false;
     }
-    console.log('INSIDE NEXT');
     slide.data.slideNumber++;
     slide.data.url = $sce.trustAsResourceUrl(slide.data.baseUrl + slide.data.slideNumber);
     return true;
