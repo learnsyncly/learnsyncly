9570c9a43efac122c63fbc3ff35657fd312daefd
Jefri Vanegas <vjefri@gmail.com>
2016-01-12 09:55:41 -0800

    (fix) removed github auth that wasn't working

--- src/server/config.js
+++ src/server/config.js
@@ -3,9 +3,6 @@ module.exports = {
   MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/lsyncly',
   TOKEN_SECRET: process.env.TOKEN_SECRET || '0Z1i1000DG0GBcYnhhsi4LHa',
 
-  // OAuth 2.0
-  GITHUB_SECRET: process.env.GITHUB_SECRET || '16aa2ac815c374eeaec10c9fe96116ce863914a9',
-
   // Bcrypt
   SALT_WORK_FACTOR: 10
 };
